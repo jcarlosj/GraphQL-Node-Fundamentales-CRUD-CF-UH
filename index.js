49eb013e16d9d5e1cdf0844e619ee8747ee412b9
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-04-25 13:07:12 -0500

    [+] GraphQL > course:Define nuevo Schema y lo anida al anterior

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
 const 
     express = require( 'express' ),
     app = express(),
-    { GraphQLSchema, GraphQLObjectType, GraphQLString, graphql } = require( 'graphql' ),
+    { GraphQLSchema, GraphQLObjectType, GraphQLString, GraphQLInt, graphql } = require( 'graphql' ),
     PORT = 8080;
 
-/** Define Schema de GraphQL */
+/** Define Schemas de GraphQL */
+const courseSchema = new GraphQLObjectType({
+    name: 'Course',
+    fields: {
+        title: {
+            type: GraphQLString,
+            views: GraphQLInt
+        }
+    }
+});
+
 const schema = new GraphQLSchema({
     query: new GraphQLObjectType({
-        name: "RootQueryType",
+        name: "RootQueryType",          // Tipo que indica que es el Raiz a partir de donde se realizaran las consultas
         fields: {
             message: {
                 type: GraphQLString,
                 resolve() {
                     return 'Hola mundo!';
+                }
+            },
+            course: {
+                type: courseSchema,
+                resolve() {
+                    return { title: 'Curso de GraphQL', views: 1000 }
                 }
             }
         }
