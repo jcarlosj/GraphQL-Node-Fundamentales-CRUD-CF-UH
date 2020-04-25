2228caa53b782d58673f4e49c9fbf8efccc681b2
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-04-25 13:21:02 -0500

    [i] NPM > express-graphql: Instala

--- index.js
+++ index.js
@@ -1,49 +1,11 @@
 const 
     express = require( 'express' ),
     app = express(),
-    { GraphQLSchema, GraphQLObjectType, GraphQLString, GraphQLInt, graphql } = require( 'graphql' ),
     PORT = 8080;
-
-/** Define Schemas de GraphQL */
-const courseSchema = new GraphQLObjectType({
-    name: 'Course',
-    fields: {
-        title: {
-            type: GraphQLString,
-            views: GraphQLInt
-        }
-    }
-});
-
-const schema = new GraphQLSchema({
-    query: new GraphQLObjectType({
-        name: "RootQueryType",          // Tipo que indica que es el Raiz a partir de donde se realizaran las consultas
-        fields: {
-            message: {
-                type: GraphQLString,
-                resolve() {
-                    return 'Hola mundo!';
-                }
-            },
-            course: {
-                type: courseSchema,
-                resolve() {
-                    return { title: 'Curso de GraphQL', views: 1000 }
-                }
-            }
-        }
-    })
-});
 
 /** Rutas */
 app .get( '/', ( request, response ) => {
-    /** Habilita consulta a GraphQL */
-    graphql(            // Toda consulta retorna una Promesa
-        schema,         // Nombre del Schema
-        `{ message, course { title } }`   // Consulta (en este caso al campo llamado 'message')
-    ) .then( data => response .json( data ) )
-      .catch( error => console .log( error ) );
-
+    response .send( '<h1>Preparando API con GraphQL</h1>' );
 });
 
 /** Lanza servidor */
