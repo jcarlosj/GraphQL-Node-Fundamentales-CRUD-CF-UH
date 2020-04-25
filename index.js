fb4952e891cf05990ac5749178581eb899982a6c
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-04-25 13:36:13 -0500

    [+] GraphQL: Define Schema usando buildSchema

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 const 
     express = require( 'express' ),
     app = express(),
+    { buildSchema } = require( 'graphql' ),
     PORT = 8080;
+
+/** Define Schema */
+const theSchema = buildSchema(`
+    type Course {
+        id: ID!
+        title: String!
+        views: Int
+    }
+    type Query {
+        getCourses: [Course]
+    }
+`);
 
 /** Rutas */
 app .get( '/', ( request, response ) => {
